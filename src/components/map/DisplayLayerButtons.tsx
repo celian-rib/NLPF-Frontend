505c86c51f65b0245271d027a3474de560dfb0a5
Mathieu MARIN <mathieu.marin@epita.fr>
2024-11-15 16:21:55 +0100

    feat: add buttons to display or hide map layers

---
+++ src/components/map/DisplayLayerButtons.tsx
@@ -0,0 +1,77 @@
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+interface DisplayLayerButtonsProps {
+    activeButtons: { [key: string]: boolean };
+    setActiveButtons: React.Dispatch<React.SetStateAction<{[key: string]: boolean;}>>;
+}
+
+const DisplayLayerButtons: React.FC<DisplayLayerButtonsProps> = ({activeButtons, setActiveButtons}) => {
+
+    // Function to toggle the buttons
+    const toggleButton = (buttonKey: string) => {
+        setActiveButtons((prev) => ({
+            ...prev,
+            [buttonKey]: !prev[buttonKey],
+        }));
+    };
+
+    return (
+        <div className="fixed bottom-4 right-4 flex flex-col space-y-2 z-50 w-40">
+            <button
+                onClick={() => toggleButton("lots")}
+                className={`py-2 px-4 rounded-md shadow transition ${
+                    activeButtons.lots ? "bg-red-500 hover:bg-red-700" : "bg-fuchsia-500 hover:bg-fuchsia-700"
+                } text-white`}
+            >
+                <FontAwesomeIcon
+                    icon={activeButtons.lots ? faEyeSlash : faEye}
+                    className="mr-2"
+                />
+                Lots
+            </button>
+
+            <button
+                onClick={() => toggleButton("checkpoints")}
+                className={`py-2 px-4 rounded-md shadow transition ${
+                    activeButtons.checkpoints ? "bg-red-500 hover:bg-red-700" : "bg-purple-500 hover:bg-purple-700"
+                } text-white`}
+            >
+                <FontAwesomeIcon
+                    icon={activeButtons.checkpoints ? faEyeSlash : faEye}
+                    className="mr-2"
+                />
+                Checkpoints
+            </button>
+
+            <button
+                onClick={() => toggleButton("tractors")}
+                className={`py-2 px-4 rounded-md shadow transition ${
+                    activeButtons.tractors ? "bg-red-500 hover:bg-red-700" : "bg-indigo-500 hover:bg-indigo-700"
+                } text-white`}
+            >
+                <FontAwesomeIcon
+                    icon={activeButtons.tractors ? faEyeSlash : faEye}
+                    className="mr-2"
+                />
+                Tractors
+            </button>
+
+            <button
+                onClick={() => toggleButton("routes")}
+                className={`py-2 px-4 rounded-md shadow transition ${
+                    activeButtons.routes ? "bg-red-500 hover:bg-red-700" : "bg-sky-500 hover:bg-sky-700"
+                } text-white`}
+            >
+                <FontAwesomeIcon
+                    icon={activeButtons.routes ? faEyeSlash : faEye}
+                    className="mr-2"
+                />
+                Routes
+            </button>
+        </div>
+    );
+};
+
+export default DisplayLayerButtons;
--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,36 +1,52 @@
-import React from "react";
+import 'leaflet/dist/leaflet.css';
+import React, { useState } from "react";
 import Navbar from "../components/navbar/Navbar";
 import { MapContainer, TileLayer } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
-import 'leaflet/dist/leaflet.css';
+import DisplayLayerButtons from "../components/map/DisplayLayerButtons";
 
 const Map: React.FC = () => {
-
     const center: LatLngExpression = [44.9068, 3.9598];
     const zoom = 5;
+
+    // State for the active buttons
+    const [activeButtons, setActiveButtons] = useState<{ [key: string]: boolean }>({
+        lots: false,
+        checkpoints: false,
+        tractors: false,
+        routes: false,
+    });
 
     return (
         <>
             <Navbar />
 
-            <div className="h-screen w-full mt-32">
-                <MapContainer
-                    center={center}
-                    zoom={zoom}
-                    scrollWheelZoom={true}
-                    className="h-full w-full"
-                    preferCanvas={true}
-                    zoomControl={false}
-                    worldCopyJump={true}
-                >
-                    <TileLayer
-                        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        tileSize={512}
-                        zoomOffset={-1}
-                        detectRetina={true}
-                    />
-                </MapContainer>
+            <div className="h-screen w-full mt-32 relative">
+                <div className="absolute inset-0 z-0">
+                    <MapContainer
+                        center={center}
+                        zoom={zoom}
+                        scrollWheelZoom={true}
+                        className="h-full w-full"
+                        preferCanvas={true}
+                        zoomControl={false}
+                        worldCopyJump={true}
+                    >
+                        <TileLayer
+                            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            tileSize={512}
+                            zoomOffset={-1}
+                            detectRetina={true}
+                        />
+                    </MapContainer>
+                </div>
+
+                <DisplayLayerButtons
+                    activeButtons={activeButtons}
+                    setActiveButtons={setActiveButtons}
+                />
+
             </div>
         </>
     );
