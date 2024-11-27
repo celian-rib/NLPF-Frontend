606e07bb65c4ccad78ec182d7668e206b3fa5aec
Mathieu MARIN <mathieu.marin@epita.fr>
2024-11-27 15:08:32 +0100

    add style to popups of the map

--- src/components/map/MarkerWithPopup.tsx
+++ src/components/map/MarkerWithPopup.tsx
@@ -2,6 +2,7 @@ import { Tractor } from '../../types/Tractor';
 import { Lot } from '../../types/Lot';
 import { Marker, Popup } from 'react-leaflet';
 import { createCustomIcon } from '../../utils/mapUtils';
+import { getStatusInfo } from '../../utils/utils';
 
 interface MarkerWithPopupProps<T> {
     item: T;
@@ -32,12 +33,24 @@ const MarkerWithPopup = <T extends Lot | Tractor>({
             icon={createCustomIcon(iconName, iconColor, iconSize)}
         >
             <Popup>
-                <div>
-                    <h1>{isLot(item) ? item.lot_name : item.tractor_name}</h1>
-                    <p><strong>Status:</strong> {item.status}</p>
-                    <p><strong>Location:</strong> {item.current_checkpoint.checkpoint_name}</p>
-                    <p><strong>Departure:</strong> {item.start_checkpoint.checkpoint_name}</p>
-                    <p><strong>Arrival:</strong> {item.end_checkpoint.checkpoint_name}</p>
+                <div className="px-4 py-2 bg-white rounded-md text-center">
+                    <h1 className="text-lg font-bold text-gray-800 mb-2">
+                        {isLot(item) ? item.lot_name : item.tractor_name}
+                    </h1>
+                    <span className={`px-2 py-1 rounded ${getStatusInfo(item.status).color}`}>
+                        {getStatusInfo(item.status).text}
+                    </span>
+                    <div className="mt-3">
+                        <p className="text-sm text-gray-600 !m-0">
+                            <strong className="font-semibold">Location:</strong> {item.current_checkpoint.checkpoint_name}
+                        </p>
+                        <p className="text-sm text-gray-600 !m-0">
+                            <strong className="font-semibold">Departure:</strong> {item.start_checkpoint.checkpoint_name}
+                        </p>
+                        <p className="text-sm text-gray-600 !m-0">
+                            <strong className="font-semibold">Arrival:</strong> {item.end_checkpoint.checkpoint_name}
+                        </p>
+                    </div>
                 </div>
             </Popup>
         </Marker>
