c93a12d926975cb86e4766a2e477e8022a6ef14e
Mathieu MARIN <mathieu.marin@epita.fr>
2024-11-28 14:12:08 +0100

    feat: add slider to select volume when biding on tractors

--- src/components/stockExchange/modal/BidModal.tsx
+++ src/components/stockExchange/modal/BidModal.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { bidOnLot, bidOnTractor } from '../../../services/stockExchange';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { TractorOffer } from '../../../types/TractorOffer';
+import { LotOffer } from '../../../types/LotOffer';
 
 interface BidModalProps<T> {
     offer:  T;
@@ -9,12 +11,13 @@ interface BidModalProps<T> {
     closeModal: () => void;
 }
 
-const BidModal = <T extends { id: string, current_price: number, max_price?: number, min_price?: number }>({
+const BidModal = <T extends LotOffer | TractorOffer>({
     offer,
     offerType,
     closeModal
 }: BidModalProps<T>) => {
     const [bidAmount, setBidAmount] = useState<number>(offer.current_price);
+    const [volumeAmount, setVolumeAmount] = useState<number>(0);
 
     // Function to handle price change
     const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,12 +25,19 @@ const BidModal = <T extends { id: string, current_price: number, max_price?: num
         setBidAmount(newPrice);
     };
 
+    // Function to handle volume change
+    const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newVolume = parseFloat(e.target.value);
+        setVolumeAmount(newVolume);
+    };
+
     // Function to handle form submission
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         const data = {
             bid: bidAmount,
-            user_id: localStorage.getItem('user_id')
+            user_id: localStorage.getItem('user_id'),
+            ...(offerType === 'tractor' ? { volume: volumeAmount } : {}),
         };
         if (offerType === 'lot')
         {
@@ -79,21 +89,23 @@ const BidModal = <T extends { id: string, current_price: number, max_price?: num
                         />
                     </div>
 
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-lg font-bold">Price</label>
-                        <p className="text-3xl font-bold text-gray-700">
-                            {bidAmount} <span className="font-normal">€/km</span>
-                        </p>
-                        <input
-                            type="range"
-                            min={offerType === 'lot' ? 0.1 : offer.current_price}
-                            max={offerType === 'lot' ? offer.current_price : 1000}
-                            step="0.1"
-                            value={bidAmount}
-                            onChange={handlePriceChange}
-                            className="range w-full mt-2"
-                        />
-                    </div>
+                    {(offerType === 'tractor' && offer.volume) && (   
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-lg font-bold">Volume</label>
+                            <p className="text-3xl font-bold text-gray-700">
+                                {volumeAmount} <span className="font-normal">m³</span>
+                            </p>
+                            <input
+                                type="range"
+                                min={0}
+                                max={offer.volume}
+                                step="0.1"
+                                value={volumeAmount}
+                                onChange={handleVolumeChange}
+                                className="range w-full mt-2"
+                            />
+                        </div>
+                    )}
 
                     <div className="flex justify-center mt-8">
                         <button
